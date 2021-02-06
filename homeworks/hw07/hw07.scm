cc2d425fae6f55cf097706f9b9e7feb8daa2240b
Dmitry Moiseev <mdmitry94@gmail.com>
2021-02-06 10:25:20 +0200

    HW 07

--- homeworks/hw07/hw07.scm
+++ homeworks/hw07/hw07.scm
@@ -2,23 +2,27 @@
   (cdr (cdr s)))
 
 (define (cadr s)
-  'YOUR-CODE-HERE
-)
+    (car (cdr s)))
 
 (define (caddr s)
-  'YOUR-CODE-HERE
-)
+  (car (cdr (cdr s))))
 
 (define (sign x)
-  'YOUR-CODE-HERE
-)
+  (cond ((> x 0) 1)
+        ((< x 0) -1)
+        (else 0)))
 
 (define (square x) (* x x))
 
 (define (pow b n)
-  'YOUR-CODE-HERE
-)
+  (cond
+    ((zero? n) 1)
+    ((even? n) (square (pow b (/ n 2))))
+    ((odd? n) (* b (pow b (- n 1))))))
 
 (define (ordered? s)
-  'YOUR-CODE-HERE
-)
+  (cond
+      ((null? lst) true)
+      ((null? (cdr lst)) true)
+      ((< (car lst) (cadr lst)) (ordered? (cdr lst)))
+      (else false)))
